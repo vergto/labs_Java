27a860e53202ff1336afed747f1b7f270d615a64
vergto <berkut-penza@inbox.ru>
2022-02-16 22:26:02 +0300

    Сделал 2ую лабу

--- src/com/company/laboratorka2/Main.java
+++ src/com/company/laboratorka2/Main.java
@@ -14,6 +14,48 @@ public class Main {
             second[i] = temp;
         }
     }
+    public static int[][] convert(int[][] b) {
+        int[][] temp = new int[b[0].length][b.length];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                temp[i][j]=b[b[i].length-j-1][i];
+            }
+        }
+        return temp;
+    }
+    private static void reverse(int[][] mas, int n){
+        int tape;
+        for (int i = 0; i < mas.length; i++) {
+            int j = 0; // От начала
+            int k = n; // До N
+            while (j<k){
+                tape = mas[i][j];
+                mas[i][j] = mas[i][k];
+                mas[i][k] = tape;
+                ++j;
+                --k;
+            }
+        }
+    }
+    private static void reverse(int[][] mas, int n, int x){
+        int tape;
+        for (int i = 0; i < mas.length; i++) {
+            int j = x; // От начала
+            int k = n; // До N
+            while (j<k){
+                tape = mas[i][j];
+                mas[i][j] = mas[i][k];
+                mas[i][k] = tape;
+                ++j;
+                --k;
+            }
+        }
+    }
+    private static void shift(int[][] mas, int n, int k){
+        reverse(mas, n-k-1); // Тут с 0 до K-1. Ниже с К до N
+        reverse(mas, n-1, n-k); // Блин, ну тут перегрузка методов, иначе свою простую реализацию придется усложнять
+        reverse(mas, n-1); // Тут с 0 до N.
+    }
     public static void main(String[] args) {
         // Лабораторная работа 2 - Вариант 1 - Задание 1
         // В приведенных ниже заданиях необходимо вывести внизу фамилию разработчика, дату и время получения задания,
@@ -21,7 +63,6 @@ public class Main {
         Date date = new Date();
         System.out.println("Фамилия разработчика: Авдонин. Дата получения 13.02.2022");
         System.out.println("Дата сдачи: " + date.toString());
-
         // Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
         System.out.println("Введите n строк через пробел, как надоест нажмите enter \n");
         Scanner in = new Scanner(System.in);
@@ -72,18 +113,61 @@ public class Main {
 
         for (int out = n - 1; out >= 1; out--){  //Внешний цикл
             for (int inn = 0; inn < out; inn++){       //Внутренний цикл
-                if(mas_int[inn][k] > mas_int[inn + 1][k])               //Если порядок элементов нарушен
+                if(mas_int[inn][k] > mas_int[inn + 1][k])
                     toSwap(mas_int[inn], mas_int[inn + 1]);             //вызвать метод, меняющий местами
             }
         }
         Library.print_array(mas_int);
-
-        for (int out = n - 1; out >= 1; out--){  //Внешний цикл
-            for (int inn = 0; inn < out; inn++){       //Внутренний цикл
-                if(mas_int[k][inn] > mas_int[k + 1][inn])               //Если порядок элементов нарушен
-                    toSwap(mas_int[k], mas_int[k + 1]);             //вызвать метод, меняющий местами
+        System.out.println("Введите k-ую строку по которому производить упорядочивание: ");
+        int k_row = in.nextInt();
+        int swap_int = 0;
+        for (int x = 0; x < n; x++)
+            for (int i = 0; i < n-1; i++) {
+                if (mas_int[k_row][i] > mas_int[k_row][i + 1])
+                    for (int j = 0; j < n; j++) {
+                        swap_int = mas_int[j][i];
+                        mas_int[j][i] = mas_int[j][i + 1];
+                        mas_int[j][i + 1] = swap_int;
+                    }
             }
-        }
+        Library.print_array(mas_int);
+
+        // Лабораторная работа 2 - Вариант 2 - Задание 2
+        // Выполнить циклический сдвиг заданной матрицы на k позиций вправо (влево, вверх, вниз).
+        // Сдвиг вправо
+        System.out.println("Сдвиг вправо, введите k позиций сдвига:");
+        int k_shift = in.nextInt();
+        shift(mas_int, n, k_shift); // Тут происходит магия
+        Library.print_array(mas_int);
+
+        // Сдвиг влево
+        System.out.println("Сдвиг влево, введите k позиций сдвига:");
+        k_shift = in.nextInt();
+        mas_int = convert(mas_int); // А тут уличная магия
+        mas_int = convert(mas_int);
+        shift(mas_int, n, k_shift);
+        mas_int = convert(mas_int);
+        mas_int = convert(mas_int);
+        Library.print_array(mas_int);
+
+        // Сдвиг вверх
+        System.out.println("Сдвиг вверх, введите k позиций сдвига:");
+        k_shift = in.nextInt();
+        mas_int = convert(mas_int); // и тут уличная магия
+        shift(mas_int, n, k_shift);
+        mas_int = convert(mas_int);
+        mas_int = convert(mas_int);
+        mas_int = convert(mas_int);
+        Library.print_array(mas_int);
+
+        // Сдвиг вниз
+        System.out.println("Сдвиг вниз, введите k позиций сдвига:");
+        k_shift = in.nextInt();
+        mas_int = convert(mas_int); // все еще она
+        mas_int = convert(mas_int);
+        mas_int = convert(mas_int);
+        shift(mas_int, n, k_shift);
+        mas_int = convert(mas_int);
         Library.print_array(mas_int);
         in.close();
     }
