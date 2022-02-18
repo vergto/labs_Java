702cf3286867a2912b414ade729139a31b8887e5
vergto <berkut-penza@inbox.ru>
2022-02-18 20:15:49 +0300

    3 лаба 1 вариант

---
+++ src/com/company/laboratorka3/Main.java
@@ -0,0 +1,37 @@
+package com.company.laboratorka3;
+
+import com.company.Library;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|_|_|_|_|_|_|_|_|");
+        int n = 4;
+        int x,y;
+        //  Лабораторная работа 3 - Вариант 1 - Задание 1
+        MyVector vector = new MyVector(n);
+        //vector.sum(x, y);
+        Library.print_array(vector.getVector_date_mas());
+        //Вывод Сложения
+        System.out.println(Arrays.toString(vector.get_sum(0, 1)));
+        //Вывод вычитания
+        System.out.println(Arrays.toString(vector.get_subtract(2, 3)));
+        //Вывод инкремента вектора
+        System.out.println(Arrays.toString(vector.get_inc(0)));
+        //Вывод инкремента вектора
+        System.out.println(Arrays.toString(vector.get_dec(0)));
+        //Вывод индекса указанного вектора
+        System.out.println(vector.get_index(1, 3));
+        //Вывод скалярного произведения
+        System.out.println(vector.get_scalar(0, 1));
+        //Вывод длины вектора
+        System.out.println(vector.get_length(1));
+        //Угол векторов
+        System.out.println(vector.get_corner(0, 1));
+
+        //  Лабораторная работа 3 - Вариант 1 - Задание 2
+       // System.out.println(vector.get_sum());
+    }
+}
---
+++ src/com/company/laboratorka3/MyVector.java
@@ -0,0 +1,107 @@
+package com.company.laboratorka3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyVector {
+    private int[][] vector_date_mas;
+    private Object[] vector_mas;
+    private int last_obj;
+
+    public MyVector(){
+        int n = 4;
+        this.vector_date_mas = new int[2][n];
+        for (int i = 0; i < n; i++) {
+            vector_date_mas[0][i] = (int) (Math.random() * 10);
+            vector_date_mas[1][i] = (int) (Math.random() * 10);
+        }
+        this.vector_mas = new Object[n];
+        for (int i = 0; i < n; i++) {
+            vector_mas[i] = new int[2];
+            vector_date_mas[0][i] = (int) (Math.random() * 10);
+            vector_date_mas[1][i] = (int) (Math.random() * 10);
+        }
+    }
+
+    public MyVector(int n){
+        this.vector_date_mas = new int[2][n];
+        for (int i = 0; i < n; i++) {
+            vector_date_mas[0][i] = (int) (Math.random() * 10);
+            vector_date_mas[1][i] = (int) (Math.random() * 10);
+        }
+        this.vector_mas = new Object[n];
+        for (int i = 0; i < n; i++) {
+            vector_mas[i] = new int[2];
+            vector_date_mas[0][i] = (int) (Math.random() * 10);
+            vector_date_mas[1][i] = (int) (Math.random() * 10);
+        }
+    }
+
+    public int[][] getVector_date_mas() {
+        return vector_date_mas;
+    }
+
+    @Override
+    public String toString() {
+        return "MyVector{" +
+                "vector_date_mas=" + Arrays.toString(vector_date_mas) +
+                '}';
+    }
+
+    public int[] get_sum(int x, int y){
+        int[] sum = new int[2];
+                sum[0] = vector_date_mas[0][x] + vector_date_mas[0][y];
+                sum[1] = vector_date_mas[1][x] + vector_date_mas[1][y];
+        return sum;
+    }
+
+    public int[] get_subtract(int x, int y){
+        int[] sum = new int[2];
+        sum[0] = vector_date_mas[0][x] - vector_date_mas[0][y];
+        sum[1] = vector_date_mas[1][x] - vector_date_mas[1][y];
+        return sum;
+    }
+
+    public int[] get_inc(int x){
+        int[] sum = new int[2];
+        sum[0] = vector_date_mas[0][x] +1;
+        sum[1] = vector_date_mas[1][x] +1;
+        return sum;
+    }
+
+    public int[] get_dec(int x){
+        int[] sum = new int[2];
+        sum[0] = vector_date_mas[0][x] -1;
+        sum[1] = vector_date_mas[1][x] -1;
+        return sum;
+    }
+
+    public int get_index(int x, int y){
+        int index = vector_date_mas[x][y];
+        return index;
+    }
+    public int get_scalar(int x, int y){
+        int[] sum = new int[2];
+        int scalar = 0;
+        sum[0] = vector_date_mas[0][x] * vector_date_mas[0][y];
+        sum[1] = vector_date_mas[1][x] * vector_date_mas[1][y];
+        scalar = sum[0] + sum[1];
+        return scalar;
+    }
+    public double get_length(int x){
+        double leng = 0;
+        leng = Math.sqrt(vector_date_mas[0][x]*vector_date_mas[0][x]+vector_date_mas[1][x]*vector_date_mas[1][x]);
+        return leng;
+    }
+
+    public double get_corner(int x, int y){
+        double scal = get_scalar(x , y);
+        double modul1 = get_length(x);
+        double modul2 =get_length(y);
+        double corne = scal/(modul1*modul2);
+        return corne;
+    }
+
+
+
+}
